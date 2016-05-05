e864458b4dd44abe1bde32f3ecb86e7f5da324dd
Jmor72 <jaren.morris@gmail.com>
2016-05-04 22:51:22 -0600

    Update 1 - SeedData w/ DBContext
    Update 1 - SeedData w/ DBContext

---
+++ src/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetService<ApplicationDbContext>();
+
+            if (context.Database == null)
+            {
+                throw new Exception("DB is null");
+            }
+
+            if (context.ShittyEmployee.Any())
+            {
+                return; //DB has been seeded
+            }
+
+
+            context.ShittyEmployee.AddRange(
+                new ShittyEmployee
+                {
+                    ID = 1,
+                    Name = "Shitty Employee Name 1",
+                    Salary = 60,
+                    Employed = 5,
+                    Address = "1234 Fake St.",
+                    Department = "the dept.",
+                    Supervisor = "Rick James"
+                }               
+            );         
+            context.SaveChanges();
+        }
+    }
+}
--- src/MvcMovie/Startup.cs
+++ src/MvcMovie/Startup.cs
@@ -108,9 +108,15 @@ namespace MvcMovie
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            SeedData.Initialize(app.ApplicationServices);
         }
 
         // Entry point for the application.
         public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+        public Startup()
+        {
+
+        }
     }
 }
